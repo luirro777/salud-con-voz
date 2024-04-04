6798690cd5ece6359cbc7182e6fbdddcb06b4c25
Pano Valdez <marianovaldez92@protonmail.com>
2024-04-04 14:23:37 -0300

    solucion a problema de postgres

--- app/config/settings.py
+++ app/config/settings.py
@@ -75,21 +75,14 @@ WSGI_APPLICATION = "config.wsgi.application"
 # Database
 # https://docs.djangoproject.com/en/5.0/ref/settings/#databases
 
-# DATABASES = {
-#     "default": {
-#         "ENGINE": "django.db.backends.postgresql",
-#         "HOST": os.environ.get('POSTGRES_HOST'),
-#         "PORT": os.environ.get('POSTGRES_PORT'),
-#         "NAME": os.environ.get('POSTGRES_DB'),
-#         "USER": os.environ.get('POSTGRES_USER'),
-#         "PASSWORD": os.environ.get('POSTGRES_PASSWORD')
-#     }
-# }
-
 DATABASES = {
     "default": {
-        "ENGINE": "django.db.backends.sqlite3",
-        "NAME": BASE_DIR / "db.sqlite3",
+        "ENGINE": "django.db.backends.postgresql",
+        "HOST": os.environ.get('POSTGRES_HOST'),
+        "PORT": os.environ.get('POSTGRES_PORT'),
+        "NAME": os.environ.get('POSTGRES_DB'),
+        "USER": os.environ.get('POSTGRES_USER'),
+        "PASSWORD": os.environ.get('POSTGRES_PASSWORD')
     }
 }
 
